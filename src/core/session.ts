dc1c5f7b138046d45d9f1edc82351f113e924a3d
azizjon003 <aliqulovazizjon68@gmail.com>
2024-10-24 09:36:54 +0500

    chore: Add telegraf-postgres-session package to package.json and remove unused main.service.js file

--- src/core/session.ts
+++ src/core/session.ts
@@ -2,3 +2,15 @@ import { session as memorySession } from "telegraf";
 const session = memorySession({});
 
 export default session;
+
+// const PostgresSession = require("telegraf-postgres-session");
+// // const session = memorySession({});
+
+// const session = new PostgresSession({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
+// export default session;
